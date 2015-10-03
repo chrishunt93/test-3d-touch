602670e571680a22280c089f4fc6f35d24678fdb
chrishunt93 <chris.hunt.93@gmail.com>
2015-10-03 10:32:50 -0400

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Test3DTouch
+//
+//  Created by Chris Hunt on 10/3/15.
+//  Copyright © 2015 Chris Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: Properties
+    
+    //var alertController: UIAlertController?
+    
+    @IBOutlet weak var screenTitle: UILabel!
+
+    @IBOutlet weak var currentPressureValue: UILabel!
+    
+    @IBOutlet weak var targetView: PushTargetView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad(  )
+        /*checking if device has 3D touch at all*/
+        //if traitCollection.forceTouchCapability != UIForceTouchCapability.Available {
+        //    alertController = UIAlertController( title: "3D Touch Unavailable", message: "Unsupported Device.", preferredStyle: .Alert )
+        //}
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    * Overriding how the view handles touch events
+    */
+    /*
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let targetTouches: Set<UITouch> = event!.touchesForView( targetView )!
+        let singleTouch: UITouch = targetTouches.first!
+        let val = singleTouch.force / singleTouch.maximumPossibleForce
+        currentPressureValue.text = String( val )
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesMoved(touches, withEvent: event )
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesEnded(touches, withEvent: event)
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        super.touchesCancelled(touches, withEvent: event)
+    }
+*/
+}
+
